3326c1e7890d08c4fa10438622374c65724b92b4
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2026-05-29 14:58:55 -0300

    feat: norminette ft_calloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/29 11:39:55 by daniel            #+#    #+#             */
-/*   Updated: 2026/05/29 11:39:58 by daniel           ###   ########.fr       */
+/*   Created: 2026/05/29 14:52:07 by daniel            #+#    #+#             */
+/*   Updated: 2026/05/29 14:52:09 by daniel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t n, size_t size)
+char *ft_strdup(const char *s)
 {
-	void	*ptr;
-	size_t	total;
-
-	if (n != 0 && (size > __SIZE_MAX__ / n))
-		return (NULL);
-	total = n * size;
-	ptr = malloc(total);
-	if (!ptr)
-		return (NULL);
-	ft_memset(ptr, 0, total);
-	return (ptr);
+		size_t	len;
 }
