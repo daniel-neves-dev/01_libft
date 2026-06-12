ce68da236428698233eff703c4be9c2c270589ef
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2026-06-12 10:18:41 -0300

    feat: added ft_lstnew list fucntion

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dneves-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/12 09:30:20 by dneves-d          #+#    #+#             */
+/*   Updated: 2026/06/12 09:30:23 by dneves-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	new_node->content = content;
+	if (new_node == NULL)
+		return (NULL);
+	return (new_node);
+}
