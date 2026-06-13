b3e3e7efddcf6ff4486b43958113df93c5081c2a
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2026-06-13 12:23:08 -0300

    feat: added ft_lstclear list function

---
+++ ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dneves-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 11:11:07 by dneves-d          #+#    #+#             */
+/*   Updated: 2026/06/13 11:11:09 by dneves-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*aux;
+
+	if (lst == NULL || *lst == NULL || del == NULL)
+		return ;
+	while (*lst != NULL)
+	{
+		aux = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = aux;
+	}
+	free(*lst);
+}
